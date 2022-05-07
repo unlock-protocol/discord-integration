9c9d5f75ace33ec29eac8d02804c5a6b897389fd
searchableguy <me@searchableguy.com>
2022-05-07 20:00:47 +0530

    feat: Change lock to polygon only

--- src/config.ts
+++ src/config.ts
@@ -3,20 +3,14 @@ export const paywallConfig = {
   network: 1,
   pessimistic: true,
   locks: {
-    "0xB0114bbDCe17e0AF91b2Be32916a1e236cf6034F": {
+    "0x08B3Fba858BbDcD5Cb0914c265260911Bdbbd5b9": {
       name: "Unlock Community",
-      network: 1,
-    },
-    "0xac1fceC2e4064CCd83ac8C9B0c9B8d944AB0D246": {
-      name: "Unlock Community",
-      network: 100,
+      network: 137,
     },
   },
   icon: "https://raw.githubusercontent.com/unlock-protocol/unlock/master/design/brand/1808-Unlock-Identity_Unlock-WordMark.svg",
   callToAction: {
-    default: `Get an Unlock membership to access our Discord, blog comments and more!
-  
-        If gas prices are high, pick the xDAI membership and receive an airdrop!`,
+    default: `Get an Unlock membership to access our Discord, blog comments and more! Use the claim button if you cannot pay for gas.`,
   },
 };
 
