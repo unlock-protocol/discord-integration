bbade3e96877c4488e82099487ef0b8344311348
Julien Genestoux <julien.genestoux@gmail.com>
2023-10-10 08:49:29 -0400

    updated instructions

--- src/index.ts
+++ src/index.ts
@@ -427,7 +427,7 @@ fastify.addHook("onReady", async () => {
           .setEmoji("🔐")
       ); */
       await channel.send({
-        content: `Hello ${member.user}! Are you ready to be a part of the Unlock Community? To begin, type /unlock and press return.`,
+        content: `Hello ${member.user}! Are you ready to be a part of the Unlock Community? To begin, type \`/unlock\` (don't forget the \`/\`) and press return.`,
         // components: [row],
       });
     });
