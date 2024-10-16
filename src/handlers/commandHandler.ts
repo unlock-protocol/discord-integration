3badaabb2285fbbb4153201b198024a9292d668d
Julien Genestoux <julien.genestoux@gmail.com>
2024-10-16 11:23:25 -0400

    more DRY

--- src/handlers/commandHandler.ts
+++ src/handlers/commandHandler.ts
@@ -1,8 +1,5 @@
 import { CommandInteraction, GuildMemberRoleManager } from "discord.js";
-import { User } from "../database";
-import { config } from "../config";
-import { hasMembership } from "../unlock";
-import showCheckout from "../showCheckout";
+import unlockHandler from "./handleUnlock";
 
 export async function unlockCommandHandler(interaction: CommandInteraction) {
   if (interaction.commandName === "ping") {
@@ -10,50 +7,13 @@ export async function unlockCommandHandler(interaction: CommandInteraction) {
       ephemeral: true,
       content: "Pong!",
     });
-  }
-  if (interaction.commandName === "unlock") {
-    await interaction.deferReply({
+  } else if (interaction.commandName === "unlock") {
+    return unlockHandler(interaction);
+  } else {
+    return interaction.reply({
       ephemeral: true,
+      content: "Please, type `/unlock`!",
     });
-
-    const user = await User.findOne({
-      where: {
-        id: interaction.member?.user.id,
-      },
-    });
-
-    if (!user) {
-      await showCheckout(interaction);
-      return;
-    }
-
-    const { walletAddresses } = user?.toJSON();
-
-    for (const walletAddress of walletAddresses) {
-      const validMembership = await hasMembership(walletAddress);
-
-      if (validMembership) {
-        let role = interaction.guild?.roles.cache.get(config.roleId);
-
-        if (!role) {
-          const fetchedRole = await interaction.guild?.roles.fetch(
-            config.roleId
-          );
-          role = fetchedRole!;
-        }
-
-        await (interaction.member!.roles as GuildMemberRoleManager).add(role);
-
-        await interaction.editReply({
-          content: `You already have a valid Unlock Membership. Welcome to Unlock Community, ${
-            interaction.member!.user
-          }. You can start sending messages now. Head over to <#1052336574211305574> and tell us a little more about yourself.`,
-        });
-        return;
-      }
-    }
-    // If the user exists but does not have a valid membership
-    await showCheckout(interaction);
   }
 }
 
---
+++ src/handlers/handleUnlock.ts
@@ -0,0 +1,56 @@
+import {
+  ButtonInteraction,
+  CommandInteraction,
+  GuildMemberRoleManager,
+} from "discord.js";
+import { User } from "../database";
+import { config } from "../config";
+import { hasMembership } from "../unlock";
+import showCheckout from "../showCheckout";
+
+export const unlockHandler = async (
+  interaction: ButtonInteraction | CommandInteraction
+) => {
+  await interaction.deferReply({
+    ephemeral: true,
+  });
+
+  const user = await User.findOne({
+    where: {
+      id: interaction.member?.user.id,
+    },
+  });
+
+  if (!user) {
+    await showCheckout(interaction);
+    return;
+  }
+
+  const { walletAddresses } = user?.toJSON();
+
+  for (const walletAddress of walletAddresses) {
+    const validMembership = await hasMembership(walletAddress);
+
+    if (validMembership) {
+      let role = interaction.guild?.roles.cache.get(config.roleId);
+
+      if (!role) {
+        const fetchedRole = await interaction.guild?.roles.fetch(config.roleId);
+        role = fetchedRole!;
+      }
+
+      await (interaction.member!.roles as GuildMemberRoleManager).add(role);
+
+      await interaction.editReply({
+        content: `You already have a valid Unlock Membership. Welcome to Unlock Community, ${
+          interaction.member!.user
+        }. You can start sending messages now. Head over to <#1052336574211305574> and tell us a little more about yourself.`,
+      });
+      return;
+    }
+  }
+  // If the user exists but does not have a valid membership
+  await showCheckout(interaction);
+};
+
+export default unlockHandler;
