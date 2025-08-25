3fa0b5c454f348743ed0d63a5ce79e42e69467cc
Julien Genestoux <17735+julien51@users.noreply.github.com>
2025-08-25 10:55:51 -0400

    more rescue

--- src/handlers/handleUnlock.ts
+++ src/handlers/handleUnlock.ts
@@ -11,55 +11,63 @@ import showCheckout from "../showCheckout";
 export const unlockHandler = async (
   interaction: ButtonInteraction | CommandInteraction
 ) => {
-  await interaction.deferReply({
-    ephemeral: true,
-  });
+  try {
+    await interaction.deferReply({
+      ephemeral: true,
+    });
 
-  const user = await User.findOne({
-    where: {
-      id: interaction.member?.user.id,
-    },
-  });
+    const user = await User.findOne({
+      where: {
+        id: interaction.member?.user.id,
+      },
+    });
 
-  if (!user) {
-    await showCheckout(interaction);
-    return;
-  }
+    if (!user) {
+      await showCheckout(interaction);
+      return;
+    }
 
-  const { walletAddresses } = user?.toJSON();
+    const { walletAddresses } = user?.toJSON();
 
-  for (const walletAddress of walletAddresses) {
-    const roles = await rolesForUserAddress(walletAddress);
+    for (const walletAddress of walletAddresses) {
+      const roles = await rolesForUserAddress(walletAddress);
 
-    // Adding the role(s) to the user
-    for (const roleId of roles) {
-      let role = interaction.guild?.roles.cache.get(roleId);
-      if (!role) {
-        const fetchedRole = await interaction.guild?.roles.fetch(roleId);
-        role = fetchedRole!;
+      // Adding the role(s) to the user
+      for (const roleId of roles) {
+        let role = interaction.guild?.roles.cache.get(roleId);
+        if (!role) {
+          const fetchedRole = await interaction.guild?.roles.fetch(roleId);
+          role = fetchedRole!;
+        }
+        await (interaction.member!.roles as GuildMemberRoleManager).add(role);
       }
-      await (interaction.member!.roles as GuildMemberRoleManager).add(role);
-    }
 
-    // Prepare message based on the highest role
-    if (roles.includes?.(config.primeRole)) {
-      await interaction.editReply({
-        content: `You have a valid Unlock Prime Membership. Thank you ${
-          interaction.member!.user
-        }.`,
-      });
-      return;
-    } else if (roles.includes?.(config.communityRole)) {
-      await interaction.editReply({
-        content: `You have a valid Unlock Membership. Welcome to the Unlock Community, ${
-          interaction.member!.user
-        }. You can start sending messages now. Head over to <#1052336574211305574> and tell us a little more about yourself.`,
-      });
-      return;
+      // Prepare message based on the highest role
+      if (roles.includes?.(config.primeRole)) {
+        await interaction.editReply({
+          content: `You have a valid Unlock Prime Membership. Thank you ${
+            interaction.member!.user
+          }.`,
+        });
+        return;
+      } else if (roles.includes?.(config.communityRole)) {
+        await interaction.editReply({
+          content: `You have a valid Unlock Membership. Welcome to the Unlock Community, ${
+            interaction.member!.user
+          }. You can start sending messages now. Head over to <#1052336574211305574> and tell us a little more about yourself.`,
+        });
+        return;
+      }
     }
+    // If the user exists but does not have a valid membership
+    await showCheckout(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.editReply({
+      content:
+        "It looks like the bot encountered an error while processing your request. Please try again!",
+    });
   }
-  // If the user exists but does not have a valid membership
-  await showCheckout(interaction);
 };
 
 export default unlockHandler;
